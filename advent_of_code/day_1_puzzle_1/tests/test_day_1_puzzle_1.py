24971e92f3e8f058991d1f52b1df5f4b033724d4
Sam Billmore <sam.billmore@benevolent.ai>
2021-12-01 15:25:32 +0000

    Solution to day 1 puzzle 1

--- advent_of_code/day_1_puzzle_1/tests/test_day_1_puzzle_1.py
+++ advent_of_code/day_1_puzzle_1/tests/test_day_1_puzzle_1.py
@@ -1,7 +1,13 @@
-def test_day_1_puzzle_1():
+from advent_of_code.day_1_puzzle_1.solution.day_1_puzzle_1 import count_depth_increase_from_previous_measurement
+
+
+def test_count_depth_increase_from_previous_measurement():
     # Given some input data
+    test_data = [1, 2, 5, 4, 6]
 
     # When we run the function
+    actual = count_depth_increase_from_previous_measurement(test_data)
 
     # Then the result is as expected
-    pass
+    expected = 3
+    assert actual == expected
