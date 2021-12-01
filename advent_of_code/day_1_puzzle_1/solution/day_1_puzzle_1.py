4134b2bb283371dd9d232c155d775f2753cd74f6
Sam Billmore <sam.billmore@benevolent.ai>
2021-12-01 13:16:18 +0000

    Restructure repo setup

---
+++ advent_of_code/day_1_puzzle_1/solution/day_1_puzzle_1.py
@@ -0,0 +1,7 @@
+from typing import List
+
+
+def count_depth_increase_from_previous_measurement(list_of_measurements: List) -> int:
+    """ 
+    """
+    pass
---
+++ advent_of_code/day_1_puzzle_1/tests/test_day_1_puzzle_1.py
@@ -0,0 +1,7 @@
+def test_day_1_puzzle_1():
+    # Given some input data
+
+    # When we run the function
+
+    # Then the result is as expected
+    pass
