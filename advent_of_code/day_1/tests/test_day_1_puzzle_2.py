3857c0ae7b183c288ac960a1216ad1daa4582ec1
Sam Billmore <sam.billmore@benevolent.ai>
2021-12-02 10:01:58 +0000

    Solution to day 2 puzzle 1

--- advent_of_code/day_1/tests/test_day_1_puzzle_2.py
+++ advent_of_code/day_1/tests/test_day_1_puzzle_2.py
@@ -4,7 +4,8 @@ from advent_of_code.day_1.solutions.day_1_puzzle_2 import three_measurement_wind
 def test_three_measurement_windows():
     # Given some input data
     test_data = [1, 2, 5, 4, 6, 0, 0, 0, 5]
-    #                  8  11 15 10 6  0  5
+    # 3 measure sum    8  11 15 10 6  0  5
+    # Count               1  1           1
 
     # When we run the function
     actual = three_measurement_windows(test_data)
--- advent_of_code/day_2/tests/test_day_2_puzzle_1.py
+++ advent_of_code/day_2/tests/test_day_2_puzzle_1.py
@@ -1,7 +1,15 @@
+from advent_of_code.day_2.solutions.day_2_puzzle_1 import final_position
+
+
 def test_final_position():
     # Given some input data
+    test_data = ['forward 5','down 7','forward 8','forward 1','forward 1','down 1','down 9','up 4']
 
     # When the function is run
+    actual = final_position(test_data)
 
     # Then the output is as expected
-    pass
+    expected_depth = 5 + 8 + 1 + 1
+    expected_horizontal = 7 + 1 + 9 - 4
+    expected = expected_depth * expected_horizontal
+    assert actual == expected
