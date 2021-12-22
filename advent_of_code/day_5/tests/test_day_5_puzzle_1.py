41d0b050283b3cc4d0845f7b721eba97617f7952
Sam Billmore <sam.billmore@benevolent.ai>
2021-12-22 12:57:40 +0000

    Updating test comments

--- advent_of_code/day_5/tests/test_day_5_puzzle_1.py
+++ advent_of_code/day_5/tests/test_day_5_puzzle_1.py
@@ -3,13 +3,13 @@ from advent_of_code.day_5.solutions.day_5_puzzle_1 import data_munging
 
 
 def test_data_munging():
-    # Given
+    # Given some input data
     filepath = "./advent_of_code/data/day_5_data.txt"
     input_data = get_data_from_txt(filepath)
 
-    # When
+    # When we run the function
     actual = data_munging(input_data)
 
-    # Then
+    # Then the result is as expected
     expected = [((599,531),(599,32)),((435,904),(435,489))]
     assert actual[:2] == expected
