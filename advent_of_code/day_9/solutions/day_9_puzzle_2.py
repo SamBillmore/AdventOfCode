33291ee9b5a520a7f28e7c733423df22723c9729
Sam Billmore <sam.billmore@benevolent.ai>
2021-12-26 12:37:43 +0000

    Solution to day 9 puzzle 1

---
+++ advent_of_code/day_9/solutions/day_9_puzzle_2.py
@@ -0,0 +1,9 @@
+from advent_of_code.utils.get_data import get_data_from_txt
+
+
+if __name__ == "__main__":
+    filepath = "./advent_of_code/data/day_x_data.txt"
+    input_data = get_data_from_txt(filepath)
+    
+    output = ''
+    print(f"Final output: {output}")
