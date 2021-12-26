c678d1cd68e319f7f89f0b9398d095f1bae18452
Sam Billmore <sam.billmore@benevolent.ai>
2021-12-26 19:36:22 +0000

    Solution to day 9 puzzle 2

--- advent_of_code/day_9/solutions/day_9_puzzle_2.py
+++ advent_of_code/day_9/solutions/day_9_puzzle_2.py
@@ -1,9 +1,125 @@
+from typing import Dict, List, Tuple
+
 from advent_of_code.utils.get_data import get_data_from_txt
+from advent_of_code.day_9.solutions.day_9_puzzle_1 import data_munging
+
+
+class BasinFinder():
+
+    def __init__(self, height_map: List[List[int]]):
+        self.height_map = height_map
+        self.height_map_tracker = [[0 for x in row] for row in height_map]
+
+    def find_basins(self):
+        """
+        Iterate horizontally, then vertically through non-completed heights on tracker
+        Mark as complete on tracker
+        If not nine:
+            Mark as part of next basin
+            expand basin
+        """
+        basin_counter = 1
+        start_position = (0,0)
+        while start_position is not None:
+            start_row = start_position[0]
+            start_col = start_position[1]
+            self.mark_tracker_complete(start_position)
+            if self.height_map[start_row][start_col] != 9:
+                print(f'Expanding basin for: {start_position}. Max rows = {len(self.height_map)}')
+                self.height_map[start_row][start_col] = str(basin_counter)
+                self.expand_current_basin(start_position)
+                basin_counter += 1
+            start_position = self.find_next_start_position()
+
+    def find_next_start_position(self):
+        """
+        """
+        for row_count, row in enumerate(self.height_map_tracker):
+            for col_count, tracker_value in enumerate(row):
+                if tracker_value == 0:
+                    return (row_count, col_count)
+        return None
+
+    def expand_current_basin(self, starting_position_row_col: Tuple[int]):
+        """
+        # Expand current basin (while loop)
+        check neighbours
+            if any neighbours are not 9 then 
+                mark neighbours the same
+                Add to list of neighbours to check
+            if neighbours are 9 then mark as complete on tracker
+        Mark current location as complete on tracker
+        Move to next on list of neighbours to check
+        """
+        neighbours_to_check = self.get_neighbours(starting_position_row_col, [])
+        while len(neighbours_to_check) > 0:
+            self.get_neighbours(neighbours_to_check[0], neighbours_to_check)
+            self.mark_tracker_complete(neighbours_to_check[0])
+            neighbours_to_check = neighbours_to_check[1:]
+
+    def get_neighbours(self, starting_position_row_col: Tuple[int], neighbours_to_check: List[Tuple[int]]):
+        """
+        check neighbours
+        if any neighbours are not 9 then 
+            mark neighbours the same
+            add to list of neighbours to check
+        if neighbours are 9 then mark as complete on tracker
+        """
+        count_row = starting_position_row_col[0]
+        count_col = starting_position_row_col[1]
+        start_val = self.height_map[count_row][count_col]
+        max_row = len(self.height_map) - 1
+        max_col = len(self.height_map[0]) - 1
+
+        if count_row > 0:
+            neighbours_to_check = self.process_neighbour(count_row - 1, count_col, start_val, neighbours_to_check)
+        if count_row < max_row:
+            neighbours_to_check = self.process_neighbour(count_row + 1, count_col, start_val, neighbours_to_check)
+        if count_col > 0:
+            neighbours_to_check = self.process_neighbour(count_row, count_col - 1, start_val, neighbours_to_check)
+        if count_col < max_col:
+            neighbours_to_check = self.process_neighbour(count_row, count_col + 1, start_val, neighbours_to_check)
+        return neighbours_to_check
+
+    def process_neighbour(self, row_pos: int, col_pos: int, start_val: str, neighbours_to_check: List[Tuple[int]]):
+        if self.height_map[row_pos][col_pos] != 9:
+            if type(self.height_map[row_pos][col_pos]) == int:
+                neighbours_to_check.append((row_pos, col_pos))
+            self.height_map[row_pos][col_pos] = start_val
+        else:
+            self.mark_tracker_complete((row_pos, col_pos))
+        return neighbours_to_check
+
+    def mark_tracker_complete(self, position: Tuple[int]):
+        row_pos = position[0]
+        col_pos = position[1]
+        self.height_map_tracker[row_pos][col_pos] = 1
+
+
+def find_basin_sizes(height_map_with_basins: List) -> Dict:
+    output_dict = {}
+    for row in height_map_with_basins:
+        for basin_value in row:
+            if basin_value != 9:
+                if basin_value in output_dict.keys():
+                    output_dict[basin_value] = output_dict[basin_value] + 1
+                else:
+                    output_dict[basin_value] = 1
+    return output_dict
+
+
+def multiply_top_three_basin_sizes(basin_sizes: Dict) -> int:
+    sorted_basin_sizes = list(basin_sizes.values())
+    sorted_basin_sizes.sort(reverse=True)
+    return sorted_basin_sizes[0] * sorted_basin_sizes[1] * sorted_basin_sizes[2]
 
 
 if __name__ == "__main__":
-    filepath = "./advent_of_code/data/day_x_data.txt"
+    filepath = "./advent_of_code/data/day_9_data.txt"
     input_data = get_data_from_txt(filepath)
-    
-    output = ''
+    height_map = data_munging(input_data)
+    basin_finder = BasinFinder(height_map)
+    basin_finder.find_basins()
+    basin_sizes = find_basin_sizes(basin_finder.height_map)
+    output = multiply_top_three_basin_sizes(basin_sizes)
     print(f"Final output: {output}")
--- advent_of_code/day_9/tests/test_day_9_puzzle_2.py
+++ advent_of_code/day_9/tests/test_day_9_puzzle_2.py
@@ -1,11 +1,202 @@
+from advent_of_code.day_9.solutions.day_9_puzzle_2 import BasinFinder, find_basin_sizes, multiply_top_three_basin_sizes
 
 
-
-def test_template():
+def test_get_neighbours_simple():
     # Given some input data
-    input_data = ''
+    height_map = [
+        ['1', 1 , 9 , 9 , 9 , 4 , 3 , 2 , 1 , 0 ],
+        [ 3 , 9 , 8 , 7 , 8 , 9 , 4 , 9 , 2 , 1 ],
+        [ 9 , 8 , 5 , 6 , 7 , 8 , 9 , 8 , 9 , 2 ],
+        [ 8 , 7 , 6 , 7 , 8 , 9 , 6 , 7 , 8 , 9 ],
+        [ 9 , 8 , 9 , 9 , 9 , 6 , 5 , 6 , 7 , 8 ]
+    ]
+    height_map_tracker = [
+        [1,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0]
+    ]
+    start_location = (0,0)
+    neighbours_to_check = []
+    
+    basin_finder = BasinFinder(height_map)
+    basin_finder.height_map_tracker = height_map_tracker
 
     # When the function is called
+    actual_neighbours_to_check = basin_finder.get_neighbours(start_location, neighbours_to_check)
 
     # Then the output is as expected
-    expected = ''
+    expected_neighbours_to_check = [(1,0), (0,1)]
+    expected_height_map = [
+        ['1','1', 9 , 9 , 9 , 4 , 3 , 2 , 1 , 0 ],
+        ['1', 9 , 8 , 7 , 8 , 9 , 4 , 9 , 2 , 1 ],
+        [ 9 , 8 , 5 , 6 , 7 , 8 , 9 , 8 , 9 , 2 ],
+        [ 8 , 7 , 6 , 7 , 8 , 9 , 6 , 7 , 8 , 9 ],
+        [ 9 , 8 , 9 , 9 , 9 , 6 , 5 , 6 , 7 , 8 ]
+    ]
+    expected_height_map_tracker = [
+        [1,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0]
+    ]
+    assert actual_neighbours_to_check == expected_neighbours_to_check
+    assert basin_finder.height_map == expected_height_map
+    assert basin_finder.height_map_tracker == expected_height_map_tracker
+
+
+def test_get_neighbours_with_nine():
+    # Given some input data
+    height_map = [
+        ['1','1', 9 , 9 , 9 , 4 , 3 , 2 , 1 , 0 ],
+        [ 3 , 9 , 8 , 7 , 8 , 9 , 4 , 9 , 2 , 1 ],
+        [ 9 , 8 , 5 , 6 , 7 , 8 , 9 , 8 , 9 , 2 ],
+        [ 8 , 7 , 6 , 7 , 8 , 9 , 6 , 7 , 8 , 9 ],
+        [ 9 , 8 , 9 , 9 , 9 , 6 , 5 , 6 , 7 , 8 ]
+    ]
+    height_map_tracker = [
+        [1,1,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0]
+    ]
+    start_location = (0,1)
+    neighbours_to_check = [(1,0)]
+
+    basin_finder = BasinFinder(height_map)
+    basin_finder.height_map_tracker = height_map_tracker
+
+    # When the function is called
+    actual_neighbours_to_check = basin_finder.get_neighbours(start_location, neighbours_to_check)
+
+    # Then the output is as expected
+    expected_neighbours_to_check = [(1,0)]
+    expected_height_map = [
+        ['1','1', 9 , 9 , 9 , 4 , 3 , 2 , 1 , 0 ],
+        [ 3 , 9 , 8 , 7 , 8 , 9 , 4 , 9 , 2 , 1 ],
+        [ 9 , 8 , 5 , 6 , 7 , 8 , 9 , 8 , 9 , 2 ],
+        [ 8 , 7 , 6 , 7 , 8 , 9 , 6 , 7 , 8 , 9 ],
+        [ 9 , 8 , 9 , 9 , 9 , 6 , 5 , 6 , 7 , 8 ]
+    ]
+    expected_height_map_tracker = [
+        [1,1,1,0,0,0,0,0,0,0],
+        [0,1,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0]
+    ]
+    assert actual_neighbours_to_check == expected_neighbours_to_check
+    assert basin_finder.height_map == expected_height_map
+    assert basin_finder.height_map_tracker == expected_height_map_tracker
+
+
+def test_expand_current_basin():
+    # Given some input data
+    height_map = [
+        ['1', 1 , 9 , 9 , 9 , 4 , 3 , 2 , 1 , 0 ],
+        [ 3 , 9 , 8 , 7 , 8 , 9 , 4 , 9 , 2 , 1 ],
+        [ 9 , 8 , 5 , 6 , 7 , 8 , 9 , 8 , 9 , 2 ],
+        [ 8 , 7 , 6 , 7 , 8 , 9 , 6 , 7 , 8 , 9 ],
+        [ 9 , 8 , 9 , 9 , 9 , 6 , 5 , 6 , 7 , 8 ]
+    ]
+    height_map_tracker = [
+        [1,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0]
+    ]
+    start_location = (0,0)
+
+    basin_finder = BasinFinder(height_map)
+    basin_finder.height_map_tracker = height_map_tracker
+
+    # When the function is called
+    basin_finder.expand_current_basin(start_location)
+
+    # Then the output is as expected
+    expected_height_map = [
+        ['1','1', 9 , 9 , 9 , 4 , 3 , 2 , 1 , 0 ],
+        ['1', 9 , 8 , 7 , 8 , 9 , 4 , 9 , 2 , 1 ],
+        [ 9 , 8 , 5 , 6 , 7 , 8 , 9 , 8 , 9 , 2 ],
+        [ 8 , 7 , 6 , 7 , 8 , 9 , 6 , 7 , 8 , 9 ],
+        [ 9 , 8 , 9 , 9 , 9 , 6 , 5 , 6 , 7 , 8 ]
+    ]
+    expected_height_map_tracker = [
+        [1,1,1,0,0,0,0,0,0,0],
+        [1,1,0,0,0,0,0,0,0,0],
+        [1,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0]
+    ]
+    assert basin_finder.height_map == expected_height_map
+    assert basin_finder.height_map_tracker == expected_height_map_tracker
+
+
+def test_find_basins():
+    # Given some input data
+    height_map = [
+        [ 1 , 1 , 9 , 9 , 9 , 4 , 3 , 2 , 1 , 0 ],
+        [ 3 , 9 , 8 , 7 , 8 , 9 , 4 , 9 , 2 , 1 ],
+        [ 9 , 8 , 5 , 6 , 7 , 8 , 9 , 8 , 9 , 2 ],
+        [ 8 , 7 , 6 , 7 , 8 , 9 , 6 , 7 , 8 , 9 ],
+        [ 9 , 8 , 9 , 9 , 9 , 6 , 5 , 6 , 7 , 8 ]
+    ]
+
+    basin_finder = BasinFinder(height_map)
+
+    # When the function is called
+    basin_finder.find_basins()
+
+    # Then the output is as expected
+    expected_height_map = [
+        ['1','1', 9 , 9 , 9 ,'2','2','2','2','2'],
+        ['1', 9 ,'3','3','3', 9 ,'2', 9 ,'2','2'],
+        [ 9 ,'3','3','3','3','3', 9 ,'4', 9 ,'2'],
+        ['3','3','3','3','3', 9 ,'4','4','4', 9 ],
+        [ 9 ,'3', 9 , 9 , 9 ,'4','4','4','4','4']
+    ]
+    assert basin_finder.height_map == expected_height_map
+
+
+def test_find_basin_sizes():
+    # Given some input data
+    height_map = [
+        ['1','1', 9 , 9 , 9 ,'2','2','2','2','2'],
+        ['1', 9 ,'3','3','3', 9 ,'2', 9 ,'2','2'],
+        [ 9 ,'3','3','3','3','3', 9 ,'4', 9 ,'2'],
+        ['3','3','3','3','3', 9 ,'4','4','4', 9 ],
+        [ 9 ,'3', 9 , 9 , 9 ,'4','4','4','4','4']
+    ]
+
+    # When the function is called
+    actual = find_basin_sizes(height_map)
+
+    # Then the output is as expected
+    expected = {
+        '1': 3,
+        '2': 9,
+        '3': 14,
+        '4': 9
+    }
+    assert actual == expected
+
+
+def test_multiply_top_three_basin_sizes():
+    # Given some input data
+    basin_sizes = {
+        '1': 3,
+        '2': 9,
+        '3': 14,
+        '4': 9
+    }
+
+    # When the function is called
+    actual = multiply_top_three_basin_sizes(basin_sizes)
+
+    # Then the output is as expected
+    expected = 9 * 14 * 9
+    assert actual == expected
