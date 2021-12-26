a03ab9e6c9663bd34c890a2777f6538bd20471bd
Sam Billmore <sam.billmore@benevolent.ai>
2021-12-26 16:56:26 +0000

    Completed solution to day 9 puzzle 1

--- advent_of_code/day_9/solutions/day_9_puzzle_1.py
+++ advent_of_code/day_9/solutions/day_9_puzzle_1.py
@@ -1,9 +1,46 @@
+from typing import List
 from advent_of_code.utils.get_data import get_data_from_txt
 
 
+def data_munging(input_data: List) -> List:
+    output_data = []
+    for row in input_data:
+        row = [int(x) for x in row]
+        output_data.append(row)
+    return output_data
+
+
+def find_low_points(height_map: List[List[int]]) -> List[int]:
+    low_points = []
+    max_row = len(height_map) - 1
+    for count_row, row in enumerate(height_map):
+        max_col = len(row) - 1
+        for count_col, current_value in enumerate(row):
+            top_point = 10
+            bottom_point = 10
+            left_point = 10
+            right_point = 10
+            if count_row > 0:
+                top_point = height_map[count_row - 1][count_col]
+            if count_row < max_row:
+                bottom_point = height_map[count_row + 1][count_col]
+            if count_col > 0:
+                left_point = height_map[count_row][count_col - 1]
+            if count_col < max_col:
+                right_point = height_map[count_row][count_col + 1]
+            if current_value < min([top_point, bottom_point, left_point, right_point]):
+                low_points.append(current_value)
+    return low_points
+
+
+def calculate_risk_level_sum(low_points: List[int]) -> int:
+    return sum([x + 1 for x in low_points])
+
+
 if __name__ == "__main__":
-    filepath = "./advent_of_code/data/day_x_data.txt"
+    filepath = "./advent_of_code/data/day_9_data.txt"
     input_data = get_data_from_txt(filepath)
-    
-    output = ''
+    height_map = data_munging(input_data)
+    low_points = find_low_points(height_map)
+    output = calculate_risk_level_sum(low_points)
     print(f"Final output: {output}")
--- advent_of_code/day_9/tests/test_day_9_puzzle_1.py
+++ advent_of_code/day_9/tests/test_day_9_puzzle_1.py
@@ -1,11 +1,55 @@
+from advent_of_code.day_9.solutions.day_9_puzzle_1 import data_munging, find_low_points, calculate_risk_level_sum
 
 
-
-def test_template():
+def test_data_munging():
     # Given some input data
-    input_data = ''
+    input_data = [
+        '2199943210',
+        '3987894921',
+        '9856789892',
+        '8767896789',
+        '9899965678'
+    ]
 
     # When the function is called
+    actual = data_munging(input_data)
 
     # Then the output is as expected
-    expected = ''
+    expected = [
+        [2,1,9,9,9,4,3,2,1,0],
+        [3,9,8,7,8,9,4,9,2,1],
+        [9,8,5,6,7,8,9,8,9,2],
+        [8,7,6,7,8,9,6,7,8,9],
+        [9,8,9,9,9,6,5,6,7,8]
+    ]
+    assert actual == expected
+
+
+def test_find_low_points():
+    # Given some input data
+    height_map = [
+        [2,1,9,9,9,4,3,2,1,0],
+        [3,9,8,7,8,9,4,9,2,1],
+        [9,8,5,6,7,8,9,8,9,2],
+        [8,7,6,7,8,9,6,7,8,9],
+        [9,8,9,9,9,6,5,6,7,8]
+    ]
+
+    # When the function is called
+    actual = find_low_points(height_map)
+
+    # Then the output is as expected
+    expected = [1,0,5,5]
+    assert actual == expected
+
+
+def test_calculate_risk_level_sum():
+    # Given some input data
+    low_points = [1,0,5,5]
+
+    # When the function is called
+    actual = calculate_risk_level_sum(low_points)
+
+    # Then the output is as expected
+    expected = 15
+    assert actual == expected
