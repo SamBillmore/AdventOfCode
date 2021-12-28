c59a0660ccedf727d2805619ad13c96cc12e74b3
Sam Billmore <sam.billmore@benevolent.ai>
2021-12-28 11:43:00 +0000

    Solution to day 11 puzzle 2

--- advent_of_code/day_11/tests/test_day_11_puzzle_2.py
+++ advent_of_code/day_11/tests/test_day_11_puzzle_2.py
@@ -1,13 +1,25 @@
+from advent_of_code.day_11.solutions.day_11_puzzle_2 import DumboOctopiExtended
 
 
-
-def test_template():
+def test_find_all_octopi_flash():
     # Given some input data
-    input_data = ''
+    input_data = [
+        [5,4,8,3,1,4,3,2,2,3],
+        [2,7,4,5,8,5,4,7,1,1],
+        [5,2,6,4,5,5,6,1,7,3],
+        [6,1,4,1,3,3,6,1,4,6],
+        [6,3,5,7,3,8,5,4,7,8],
+        [4,1,6,7,5,2,4,6,4,5],
+        [2,1,7,6,8,4,1,7,2,1],
+        [6,8,8,2,8,8,1,1,3,4],
+        [4,8,4,6,8,4,8,5,5,4],
+        [5,2,8,3,7,5,1,5,2,6]
+    ]
+    octopi = DumboOctopiExtended(input_data)
 
     # When the function is called
-    actual = ''
+    actual = octopi.find_all_octopi_flash()
 
     # Then the output is as expected
-    expected = ''
+    expected = 195
     assert actual == expected
